267f243d9b735aab33ad236bd2ac73f0850a4a1a
niuhuan <niuhuancn@outlook.com>
2022-02-20 12:00:15 +0800

    temp message

--- proc_qq/src/client_handler.rs
+++ proc_qq/src/client_handler.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use rs_qq::client::event::{GroupMessageEvent, PrivateMessageEvent};
+use rs_qq::client::event::{GroupMessageEvent, PrivateMessageEvent, TempMessageEvent};
 use rs_qq::handler::{Handler, QEvent};
 
 pub struct ClientHandler {
@@ -8,63 +8,46 @@ pub struct ClientHandler {
 
 impl ClientHandler {}
 
+macro_rules! map_handlers {
+    ($self:expr,$event:expr,$process:path) => {
+        for m in &$self.modules {
+            for h in &m.handles {
+                match &h.process {
+                    $process(e) => {
+                        match e.handle(&$event).await {
+                            Ok(b) => {
+                                if b {
+                                    return;
+                                }
+                            }
+                            Err(_) => {
+                                // todo log
+                                return;
+                            }
+                        }
+                    }
+                    _ => (),
+                }
+            }
+        }
+    };
+}
+
 #[async_trait]
 impl Handler for ClientHandler {
     async fn handle(&self, e: QEvent) {
         match e {
             QEvent::GroupMessage(event) => {
                 tracing::info!(target = "proc_qq", "MESSAGE : {:?}", event.message);
-                for m in &self.modules {
-                    for h in &m.handles {
-                        match &h.process {
-                            ModuleEventProcess::GroupMessage(e) => {
-                                match e.handle(&event).await {
-                                    Ok(b) => {
-                                        if b {
-                                            return;
-                                        }
-                                    }
-                                    Err(_) => {
-                                        // todo log
-                                        return;
-                                    }
-                                }
-                            }
-                            _ => (),
-                        }
-                    }
-                }
+                map_handlers!(&self, &event, ModuleEventProcess::GroupMessage);
             }
             QEvent::PrivateMessage(event) => {
                 tracing::info!(target = "proc_qq", "MESSAGE : {:?}", event.message);
-                for m in &self.modules {
-                    for h in &m.handles {
-                        match &h.process {
-                            ModuleEventProcess::PrivateMessage(e) => {
-                                match e.handle(&event).await {
-                                    Ok(b) => {
-                                        if b {
-                                            return;
-                                        }
-                                    }
-                                    Err(_) => {
-                                        // todo log
-                                        return;
-                                    }
-                                }
-                            }
-                            _ => (),
-                        }
-                    }
-                }
+                map_handlers!(&self, &event, ModuleEventProcess::PrivateMessage);
             }
-            QEvent::TempMessage(m) => {
-                tracing::info!(
-                    target = "proc_qq",
-                    "MESSAGE (TEMP={}): {}",
-                    m.message.from_uin,
-                    m.message.elements
-                )
+            QEvent::TempMessage(event) => {
+                tracing::info!(target = "proc_qq", "MESSAGE : {:?}", event.message);
+                map_handlers!(&self, &event, ModuleEventProcess::TempMessage);
             }
             QEvent::GroupRequest(m) => {
                 tracing::info!(
@@ -101,6 +84,7 @@ pub struct ModuleEventHandler {
 pub enum ModuleEventProcess {
     GroupMessage(Box<dyn GroupMessageEventProcess>),
     PrivateMessage(Box<dyn PrivateMessageEventProcess>),
+    TempMessage(Box<dyn TempMessageEventProcess>),
 }
 
 #[async_trait]
@@ -111,4 +95,9 @@ pub trait GroupMessageEventProcess: Sync + Send {
 #[async_trait]
 pub trait PrivateMessageEventProcess: Sync + Send {
     async fn handle(&self, event: &PrivateMessageEvent) -> anyhow::Result<bool>;
+}
+
+#[async_trait]
+pub trait TempMessageEventProcess: Sync + Send {
+    async fn handle(&self, event: &TempMessageEvent) -> anyhow::Result<bool>;
 }
